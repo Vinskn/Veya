6e50459ba712247fb7339adf740841e27ecc6b48
Vinskn <chuncumaru1@gmail.com>
2026-06-26 14:06:14 +0700

    deployement: fix deployment error
    - add package tsc
    - change module to ESNext
    - exclude /dist to git

--- games/Sankhya/dataStore.ts
+++ games/Sankhya/dataStore.ts
@@ -1,5 +1,5 @@
 import type { checkAnswerResult, SankhyaStorage, startGameData, StartGamePayload } from '../../types/SankhyaType';
-import { questionMaker } from './questionMaker.ts';
+import { questionMaker } from './questionMaker';
 
 const sankhyaDataStore: SankhyaStorage = {};
 const sankhyaStartGameData: startGameData = {};
--- games/Sankhya/registerSankhya.ts
+++ games/Sankhya/registerSankhya.ts
@@ -1,6 +1,6 @@
 import { Server, Socket } from 'socket.io';
-import { addPlayer, checkAnswer, checkRoomExists, createRoom, deleteRoom, endGame, getAllRooms, getCurrentQuestion, getPlayerName, getRoomData, getRoomHost, isUserExist, removePlayer, startGame } from './dataStore.ts';
-import type { JoinRoomPayload, StartGamePayload } from '../../types/SankhyaType.ts';
+import { addPlayer, checkAnswer, checkRoomExists, createRoom, deleteRoom, endGame, getAllRooms, getCurrentQuestion, getPlayerName, getRoomData, getRoomHost, isUserExist, startGame } from './dataStore';
+import type { JoinRoomPayload, StartGamePayload } from '../../types/SankhyaType';
 
 export const registerSankhya = (io: Server) => {
   const nsp = io.of('/sankhya');
